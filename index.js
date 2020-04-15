273ed2c11e94ce0f1332d7f9ff9bbc7cff8b7d95
mikebots <61569234+mikebots@users.noreply.github.com>
2020-04-15 17:07:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ bot.categories = fs.readdirSync("./commands/");
     require(`./handlers/${handler}`)(bot);
 });
 bot.on('ready',()=>{
-    bot.user.setActivity(`BOTGHOST.COM :ghost: || ${bot.prefix}help FOR HELP`, {type: "WATCHING"}, {url: "https://discord.gg/kugRv6Y"});
+    bot.user.setActivity(`${bot.users.cache.size} users || ${bot.prefix}help FOR HELP`, {type: "WATCHING"}, {url: "https://discord.gg/kugRv6Y"});
     console.log(`${bot.user.tag} is online and has loaded\n${bot.commands.size} commands\nBot ping is ${bot.ws.ping}ms\nRun ${bot.prefix}help for any command help`)
 
 
