0bd2a7218ffb97e87616322151b9d9b158791b13
mikebots <imoussah10@gmail.com>
2020-04-16 23:18:56 -0600

    new eval command

---
+++ commands/utility/eval.js
@@ -0,0 +1,40 @@
+const owners = "470825695237636107";
+const { VultrexHaste } = require('vultrex.haste')
+const { inspect } = require("util")
+const { stripIndents } = require('common-tags')
+var haste = new VultrexHaste({ url: "https://hasteb.in" });
+
+module.exports ={
+    name: "eval",
+    aliases: ['testjscode', 'code', 'evall', 'eeval'],
+    description: "evaluate code from javascript!",
+    usage: "[p]eval <code>",
+    run: async(bot, message,args) => {
+        if(!message.author.id === owners) return message.channel.send(`For security reasons. Only the bot owner can use this command`);
+        
+        if(message.author.id === owners){
+        if(!args[0]) return message.channel.send(`Please provide JavaScript code to evaluate.\n\nUsage: **${bot.prefix}eval <code>**`)
+
+        try {
+            const start = process.hrtime();
+            let output = eval(args.join(" "));
+            const difference = process.hrtime(start);
+            if (typeof output !== "string") output = inspect(output, { depth: 2 });
+
+            return message.channel.send(stripIndents`
+               *Executed in ${difference[0] > 0 ? `${difference[0]}s ` : ""}${difference[1] / 1e6}ms*
+
+               \`\`\`js
+               ${output.length > 1950 ? await haste.post(output) : output}
+               \`\`\`
+            `)
+        } catch(err) {
+            return message.channel.send(stripIndents`
+            Error:
+            \`${err}\`
+            `)
+
+        }
+        }
+    }
+}
